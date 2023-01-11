5a813e52886e2051b778666609f09129dee07fcb
LeeYongwoo-kor <newlyo1205@naver.com>
2023-01-11 23:59:43 +0900

    Edit tailwind.config.js(Add brand color)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        highlight: "rgb(254, 125, 60)",
+        lightmain: "rgb(207, 182, 169)",
+        main: "rgb(177, 90, 60)",
+        darkmain: "rgb(233, 77, 1)",
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [require("tailwindcss")],
 };
