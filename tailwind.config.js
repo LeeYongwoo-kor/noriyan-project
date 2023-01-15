b1e0ca3d6d3bda8e13507633a268221ac48eef7b
LeeYongwoo-kor <newlyo1205@naver.com>
2023-01-15 15:21:18 +0900

    Add extends to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,44 @@ module.exports = {
     },
     extend: {
       colors: {
-        highlight: "rgb(254, 125, 60)",
-        lightmain: "rgb(207, 182, 169)",
-        main: "rgb(177, 90, 60)",
+        highlight: "rgb(254, 107, 37)",
+        lightmain: "rgb(254, 152, 103)",
+        main: "rgb(254, 125, 60)",
         darkmain: "rgb(233, 77, 1)",
+      },
+      animation: {
+        change: "change 16s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        fadein: "fadein 0.5s linear",
+        fadeout: "fadeout 0.5s linear",
+      },
+      keyframes: {
+        change: {
+          "0%, 100%": {
+            opacity: 1,
+          },
+          "50%": {
+            opacity: 0,
+          },
+        },
+        fadein: {
+          from: {
+            opacity: 0,
+          },
+          to: {
+            opacity: 1,
+          },
+        },
+        fadeout: {
+          from: {
+            opacity: 1,
+          },
+          to: {
+            opacity: 0,
+          },
+        },
+      },
+      fontFamily: {
+        mincho: ["Shippori Mincho", "serif"],
       },
       spacing: {
         128: "32rem",
@@ -27,5 +61,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss")],
+  plugins: [require("tailwindcss"), require("tailwindcss-animation-delay")],
 };
