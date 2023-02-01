f9aedcc11cd1476d1c03a5bc785daf967be2d70b
LeeYongwoo-kor <newlyo1205@naver.com>
2023-02-01 23:56:01 +0900

    Change Subtitle Components

--- components/Subtitle.tsx
+++ components/Subtitle.tsx
@@ -1,20 +1,8 @@
-import Image from "next/image";
-import { images } from "../constants/images";
-import { ubuntu } from "../fonts/fonts";
-import { cls } from "../libs/utils";
-import { Kinds } from "../pages";
-
-export default function Subtitle({ component }: Kinds) {
-  const { nameJp, nameEn, path, alt } = images.subtitle[component];
+export default function Subtitle({ text }: any) {
   return (
-    <div className="relative h-32 select-none bg-slate-200">
-      <Image src={path} alt={alt} fill className="object-cover opacity-50" />
-      <div className="flex flex-col justify-center h-full mx-auto indent-8 max-w-7xl">
-        <div className="flex flex-col space-y-2 w-fit">
-          <h1 className="text-5xl">{nameJp}</h1>
-          <h2 className={cls("text-2xl", ubuntu.className)}>&nbsp; {nameEn}</h2>
-        </div>
-      </div>
+    <div className="flex flex-col items-center justify-center my-16">
+      <hr className="h-5 border-t-[3px] border-main w-36" />
+      <h2 className="text-4xl font-extrabold">{text}</h2>
     </div>
   );
 }
