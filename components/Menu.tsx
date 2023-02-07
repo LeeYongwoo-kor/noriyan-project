d9d1729230c68412b064620798f52cf17556ac36
LeeYongwoo-kor <newlyo1205@naver.com>
2023-02-07 23:59:29 +0900

    Change state management from useState to useReducer (add showMore state) of Menu Component

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,19 +1,35 @@
 import Image from "next/image";
-import React, { useEffect, useMemo, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useState,
+} from "react";
 import { IMenu } from "../data/menu";
 import { cls } from "../libs/utils";
 import Slider from "./Slider";
 import Subtitle from "./Subtitle";
 
-interface ISubMenu {
-  food: number;
-  drink: number;
-  dessert: number;
+interface IMenuStatus {
+  kinds: number;
+  sub: number;
+  showMore: boolean;
+}
+
+interface IMenuSelected {
+  kinds: number;
+  sub: number;
+}
+
+interface IMenuState {
+  selected: IMenuSelected;
+  showStatus: IMenuStatus[];
 }
 
 const dayToMillsec = 86400000;
 const decision = 7;
-const showMax = 8;
+const showMax = 6;
 
 function isNewArrival(regdate: string) {
   try {
@@ -33,7 +49,7 @@ function isNewArrival(regdate: string) {
 }
 
 function getSubArr(menu: IMenu[]): string[][] {
-  const subMenu = menu?.reduce((acc: string[][], curr: IMenu) => {
+  const subArr = menu?.reduce((acc: string[][], curr: IMenu) => {
     const currCategory = curr["category"];
     if (!acc[currCategory]) {
       acc[currCategory] = [curr["sub"]];
@@ -48,41 +64,116 @@ function getSubArr(menu: IMenu[]): string[][] {
     return [...(acc || [])];
   }, []);
 
-  return subMenu;
+  return subArr;
+}
+
+function getInitialMenuStatus(subArr: string[][]) {
+  const menuStatus = subArr?.reduce(
+    (menuObj: IMenuStatus[], kinds: string[], kindsIdx: number) => {
+      const menu = kinds?.reduce(
+        (kindsObj: IMenuStatus[], subName: string, subIdx: number) => {
+          const subObj: IMenuStatus = {
+            kinds: kindsIdx,
+            sub: subIdx,
+            // selected: subIdx === 0 ? true : false,
+            showMore: false,
+          };
+
+          return [...(kindsObj || []), subObj];
+        },
+        []
+      );
+
+      return [...(menuObj || []), ...menu];
+    },
+    []
+  );
+
+  return menuStatus;
+}
+
+function reducer(state: IMenuState, action: any) {
+  switch (action.type) {
+    case "CHANGE_KINDS":
+      return {
+        ...state,
+        selected: {
+          ...state.selected,
+          kinds: action.idx,
+        },
+      };
+    case "CHANGE_SUB":
+      return {
+        ...state,
+        selected: {
+          ...state.selected,
+          sub: action.idx,
+        },
+      };
+    case "SHOW_MORE":
+      return {
+        ...state,
+        menuStatus: state.menuStatus.map((menu: IMenuStatus) =>
+          menu.kinds === action?.kinds && menu.sub === action?.sub
+            ? { ...menu, showMore: true }
+            : menu
+        ),
+      };
+    default:
+      return state;
+  }
 }
 
 function Menu({ innerRef, menu }: any) {
-  // 1. ふりふきだいこん明太クリーム
-  // 2. からすみみち
-  // 3. 天然ぶり刺身
-  // 4. とんぺい焼き
-  // 5. ピーザ
-  // 6. ビーフ？
-  const [selectedKinds, setSelectedKinds] = useState<number>(0);
-  // key: index of category array
-  const [selectedSub, setSelectedSub] = useState<ISubMenu>({
-    food: 0,
-    drink: 0,
-    dessert: 0,
-  });
-  const [subMenu, setSubMenu] = useState<IMenu[] | []>([]);
-  const [showMore, setShowMore] = useState(false);
-
   const category: string[] = ["food", "drink", "dessert"];
-
   const subArr = useMemo(() => getSubArr(menu), []);
 
-  const handleClickKinds = (idx: number) => {
-    setSelectedKinds(idx);
+  const initialState = {
+    selected: { kinds: 0, sub: 0 },
+    showStatus: getInitialMenuStatus(subArr),
   };
 
-  const handleClickSub = (selectedKinds: number, idx: number) => {
-    setSelectedSub((prev) => ({ ...prev, [category[selectedKinds]]: idx }));
-  };
+  // const [selectedKinds, setSelectedKinds] = useState<number>(0);
+  // const [showMore, setShowMore] = useState(false);
+  const [subMenu, setSubMenu] = useState<IMenu[] | []>([]);
 
-  const handleclickShowMore = () => {
-    setShowMore(true);
-  };
+  const [menuState, dispatch] = useReducer(reducer, initialState);
+  const { kinds, sub } = menuState.selected;
+  const { showStatus } = menuState;
+
+  // const handleClickKinds = (idx: number) => {
+  //   setSelectedKinds(idx);
+  // };
+
+  const handleClickKinds = useCallback((idx: number) => {
+    dispatch({
+      type: "CHANGE_KINDS",
+      idx,
+    });
+  }, []);
+
+  // const handleClickSub = (selectedKinds: number, idx: number) => {
+  //   setSelectedSub((prev) => ({ ...prev, [category[selectedKinds]]: idx }));
+  // };
+
+  const handleClickSub = useCallback((idx: number) => {
+    dispatch({
+      type: "CHANGE_SUB",
+      idx,
+    });
+  }, []);
+
+  // const handleclickShowMore = () => {
+  //   setShowMore(true);
+  // };
+
+  const handleClickShowMore = useCallback(() => {
+    dispatch({
+      type: "SHOW_MORE",
+      kinds,
+      sub,
+    });
+  }, [kinds, sub]);
 
   // const selectedSubMemo = useMemo<ISubMenu>(
   //   () => ({
@@ -94,14 +185,22 @@ function Menu({ innerRef, menu }: any) {
   // );
 
   useEffect(() => {
-    setSubMenu(() =>
-      menu?.filter(
-        (item: IMenu) =>
-          item?.sub ===
-          subArr[selectedKinds][selectedSub[category[selectedKinds]]]
-      )
+    const { showMore } = showStatus?.find(
+      (data) => data.kinds === kinds && data.sub === sub
     );
-  }, [selectedSub, selectedKinds]);
+
+    if (showMore) {
+      setSubMenu(() =>
+        menu?.filter((item: IMenu) => item?.sub === subArr[kinds][sub])
+      );
+    } else {
+      setSubMenu(() =>
+        menu
+          ?.filter((item: IMenu) => item?.sub === subArr[kinds][sub])
+          .slice(0, showMax)
+      );
+    }
+  }, [kinds, sub]);
 
   return (
     <article ref={innerRef} className="w-full min-h-fit">
@@ -112,32 +211,30 @@ function Menu({ innerRef, menu }: any) {
       <Subtitle text="ALL MENU" />
       <div className="flex flex-col items-center justify-center">
         <div className="flex justify-center w-full space-x-2">
-          {category.map((kinds, idx) => (
+          {category.map((kindsName, idx) => (
             <button
               onClick={() => handleClickKinds(idx)}
               className={cls(
                 "px-4 py-2 text-lg font-bold transition-colors border shadow-sm w-36 hover:text-white rounded-2xl hover:bg-main focus:outline-none",
-                selectedKinds === idx
+                kinds === idx
                   ? "text-white bg-highlight border-transparent"
                   : "border-main border-2"
               )}
               key={idx}
             >
-              {kinds.toLocaleUpperCase()}
+              {kindsName.toLocaleUpperCase()}
             </button>
           ))}
         </div>
         <div className="w-full">
           <ul>
             <li className="my-8 space-x-3 space-y-3 text-sm">
-              {subArr[selectedKinds]?.map((subItem: string, idx: number) => (
+              {subArr[kinds]?.map((subItem: string, idx: number) => (
                 <button
-                  onClick={() => handleClickSub(selectedKinds, idx)}
+                  onClick={() => handleClickSub(idx)}
                   className={cls(
                     "border px-6 h-10 rounded-xl",
-                    selectedSub[category[selectedKinds]] === idx
-                      ? "text-white bg-main"
-                      : "hover:bg-slate-200"
+                    sub === idx ? "text-white bg-main" : "hover:bg-slate-200"
                   )}
                   key={idx}
                 >
@@ -147,59 +244,32 @@ function Menu({ innerRef, menu }: any) {
             </li>
           </ul>
           <ul className="grid w-full grid-cols-2 gap-8">
-            {showMore
-              ? subMenu?.map((dish: IMenu) => (
-                  <li className="flex" key={dish.id}>
-                    <div className="relative h-48 w-44 aspect-square">
-                      <Image
-                        alt={dish.name}
-                        className="object-cover w-full rounded-3xl"
-                        src={dish.imageUrl}
-                        fill
-                        priority={true}
-                      />
-                    </div>
-                    <div className="p-10">
-                      <div className="mb-5 text-xl font-semibold">
-                        {dish.name}
-                      </div>
-                      <div className="text-lg font-medium text-main">
-                        ￥{dish.price}（税込）
-                      </div>
-                      {isNewArrival(dish.regdate) ? (
-                        <div>This is NewArrival</div>
-                      ) : null}
-                    </div>
-                  </li>
-                ))
-              : subMenu?.slice(0, showMax).map((dish: IMenu) => (
-                  <li className="flex" key={dish.id}>
-                    <div className="relative h-48 w-44 aspect-square">
-                      <Image
-                        alt={dish.name}
-                        className="object-cover w-full rounded-3xl"
-                        src={dish.imageUrl}
-                        fill
-                        priority={true}
-                      />
-                    </div>
-                    <div className="p-10">
-                      <div className="mb-5 text-xl font-semibold">
-                        {dish.name}
-                      </div>
-                      <div className="text-lg font-medium text-main">
-                        ￥{dish.price}（税込）
-                      </div>
-                      {isNewArrival(dish.regdate) ? (
-                        <div>This is NewArrival</div>
-                      ) : null}
-                    </div>
-                  </li>
-                ))}
+            {subMenu?.map((dish: IMenu) => (
+              <li className="flex" key={dish.id}>
+                <div className="relative h-48 w-44 aspect-square">
+                  <Image
+                    alt={dish.name}
+                    className="object-cover w-full rounded-3xl"
+                    src={dish.imageUrl}
+                    fill
+                    priority={true}
+                  />
+                </div>
+                <div className="p-10">
+                  <div className="mb-5 text-xl font-semibold">{dish.name}</div>
+                  <div className="text-lg font-medium text-main">
+                    ￥{dish.price}（税込）
+                  </div>
+                  {isNewArrival(dish.regdate) ? (
+                    <div>This is NewArrival</div>
+                  ) : null}
+                </div>
+              </li>
+            ))}
           </ul>
-          {!showMore && subMenu?.length > showMax ? (
+          {subMenu?.length > showMax ? (
             <div
-              onClick={handleclickShowMore}
+              onClick={handleClickShowMore}
               className="flex items-center justify-center w-full mt-8 cursor-pointer h-14 bg-slate-200 hover:bg-slate-300 rounded-xl"
             >
               <svg
