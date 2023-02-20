8e917d0b5d42555d6dfa809a372f1cd63df8e097
LeeYongwoo-kor <newlyo1205@naver.com>
2023-02-21 03:47:57 +0900

    Add NavbarMobile Component

---
+++ components/NavbarMobile.tsx
@@ -0,0 +1,156 @@
+import {
+  faCircleExclamation,
+  faImages,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { cls } from "@libs/utils";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { IPositionInfo } from "pages";
+import { useState } from "react";
+import { NavbarTypes } from "types";
+
+type NavbarMobileProps = {
+  position: IPositionInfo;
+};
+
+export default function NavbarMobile({ position }: NavbarMobileProps) {
+  const [currPosition, setCurrPosition] = useState<NavbarTypes>("home");
+  const { scrollY } = useScroll();
+
+  const handleClick = (components: NavbarTypes) => {
+    const movePosition = position[components];
+
+    window.scroll({
+      top: movePosition,
+      behavior: "smooth",
+    });
+  };
+
+  useMotionValueEvent(scrollY, "change", (scroll) => {
+    const { about, notice, menu, photoGallery, access, info } = position;
+
+    switch (true) {
+      case scroll >= about && scroll < notice:
+        setCurrPosition("about");
+        break;
+      case scroll >= notice && scroll < menu:
+        setCurrPosition("notice");
+        break;
+      case scroll >= menu && scroll < photoGallery:
+        setCurrPosition("menu");
+        break;
+      case scroll >= photoGallery && scroll < access:
+        setCurrPosition("photoGallery");
+        break;
+      case scroll >= access && scroll < info:
+        setCurrPosition("access");
+        break;
+      case scroll >= info:
+        setCurrPosition("info");
+        break;
+      default:
+        setCurrPosition("home");
+        break;
+    }
+  });
+
+  return (
+    <div className="sm:hidden">
+      <nav className="fixed bottom-0 z-20 flex w-full pt-3 pb-3 text-xs text-white divide-x divide-main justify-evenly bg-darkmain">
+        <button
+          onClick={() => handleClick("notice")}
+          className={cls(
+            "flex flex-col flex-grow justify-between items-center space-y-2",
+            currPosition.indexOf("notice") > -1
+              ? "text-orange-900"
+              : "transition-colors hover:text-white"
+          )}
+        >
+          <FontAwesomeIcon size="xl" icon={faCircleExclamation} />
+          <span>お知らせ</span>
+        </button>
+        <button
+          onClick={() => handleClick("menu")}
+          className={cls(
+            "flex flex-col flex-grow justify-between items-center space-y-2",
+            currPosition.indexOf("menu") > -1
+              ? "text-orange-900"
+              : "transition-colors hover:text-white"
+          )}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 15 15"
+            className="w-6 h-6"
+          >
+            <path
+              id="path11774"
+              d="M3.5,0l-1,5.5c-0.1464,0.805,1.7815,1.181,1.75,2L4,14c-0.0384,0.9993,1,1,1,1s1.0384-0.0007,1-1L5.75,7.5  c-0.0314-0.8176,1.7334-1.1808,1.75-2L6.5,0H6l0.25,4L5.5,4.5L5.25,0h-0.5L4.5,4.5L3.75,4L4,0H3.5z M12,0  c-0.7364,0-1.9642,0.6549-2.4551,1.6367C9.1358,2.3731,9,4.0182,9,5v2.5c0,0.8182,1.0909,1,1.5,1L10,14c-0.0905,0.9959,1,1,1,1  s1,0,1-1V0z"
+            />
+          </svg>
+          <span>メニュー</span>
+        </button>
+        <button
+          onClick={() => handleClick("photoGallery")}
+          className={cls(
+            "flex flex-col flex-grow justify-between items-center space-y-2",
+            currPosition.indexOf("photoGallery") > -1
+              ? "text-orange-900"
+              : "transition-colors hover:text-white"
+          )}
+        >
+          <FontAwesomeIcon size="2x" icon={faImages} />
+          <span>ギャラリー</span>
+        </button>
+        <button
+          onClick={() => handleClick("access")}
+          className={cls(
+            "flex flex-col flex-grow justify-between items-center space-y-2",
+            currPosition.indexOf("access") > -1
+              ? "text-orange-900"
+              : "transition-colors hover:text-white"
+          )}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span>アクセス</span>
+        </button>
+        <button
+          onClick={() => handleClick("info")}
+          className={cls(
+            "flex flex-col flex-grow justify-between items-center space-y-2",
+            currPosition.indexOf("info") > -1
+              ? "text-orange-900"
+              : "transition-colors hover:text-white"
+          )}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 007.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 004.902-5.652l-1.3-1.299a1.875 1.875 0 00-1.325-.549H5.223z" />
+            <path
+              fillRule="evenodd"
+              d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 009.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 002.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 010 1.5H2.25a.75.75 0 010-1.5H3zm3-6a.75.75 0 01.75-.75h3a.75.75 0 01.75.75v3a.75.75 0 01-.75.75h-3a.75.75 0 01-.75-.75v-3zm8.25-.75a.75.75 0 00-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 00.75-.75v-5.25a.75.75 0 00-.75-.75h-3z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span>店舗情報</span>
+        </button>
+      </nav>
+    </div>
+  );
+}
