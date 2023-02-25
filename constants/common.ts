cd8dc49585d690e71b149aef2584ba7f619ebee2
LeeYongwoo-kor <newlyo1205@naver.com>
2023-02-25 23:59:32 +0900

    Add constants

---
+++ constants/common.ts
@@ -0,0 +1,4 @@
+export const phonenumber = "0529376252";
+export const navHeight = 400;
+export const navbarMobileHeight = 64;
+export const tabletSize = 768;
---
+++ constants/images.ts
@@ -0,0 +1,57 @@
+export const images = {
+  slider: {
+    notice: [
+      "https://norisang-project.s3.ap-northeast-1.amazonaws.com/notice_restaurant_01.jpg",
+      "https://norisang-project.s3.ap-northeast-1.amazonaws.com/notice_restaurant_01.jpg",
+    ],
+    menu: [
+      "https://norisang-project.s3.ap-northeast-1.amazonaws.com/food_restaurant_01.jpg",
+      "https://norisang-project.s3.ap-northeast-1.amazonaws.com/menu_norisang_2.jpg",
+      "https://norisang-project.s3.ap-northeast-1.amazonaws.com/menu_norisang_3.jpg",
+      "https://norisang-project.s3.ap-northeast-1.amazonaws.com/menu_norisang_4.jpg",
+      "https://norisang-project.s3.ap-northeast-1.amazonaws.com/menu_norisang_5.jpg",
+    ],
+  },
+  photoGallery: [
+    {
+      id: "001",
+      src: "https://norisang-project.s3.ap-northeast-1.amazonaws.com/about_restaurant_01.jpg",
+      alt: "photo_image_01",
+    },
+    {
+      id: "002",
+      src: "https://norisang-project.s3.ap-northeast-1.amazonaws.com/about_restaurant_02.jpg",
+      alt: "photo_image_02",
+    },
+    {
+      id: "003",
+      src: "https://norisang-project.s3.ap-northeast-1.amazonaws.com/about_restaurant_03.jpg",
+      alt: "photo_image_03",
+    },
+    {
+      id: "004",
+      src: "https://norisang-project.s3.ap-northeast-1.amazonaws.com/about_restaurant_04.jpg",
+      alt: "photo_image_04",
+    },
+    {
+      id: "005",
+      src: "https://norisang-project.s3.ap-northeast-1.amazonaws.com/about_restaurant_01.jpg",
+      alt: "photo_image_05",
+    },
+    {
+      id: "006",
+      src: "https://norisang-project.s3.ap-northeast-1.amazonaws.com/about_restaurant_02.jpg",
+      alt: "photo_image_06",
+    },
+    {
+      id: "007",
+      src: "https://norisang-project.s3.ap-northeast-1.amazonaws.com/about_restaurant_03.jpg",
+      alt: "photo_image_07",
+    },
+    {
+      id: "008",
+      src: "https://norisang-project.s3.ap-northeast-1.amazonaws.com/about_restaurant_04.jpg",
+      alt: "photo_image_08",
+    },
+  ],
+};
