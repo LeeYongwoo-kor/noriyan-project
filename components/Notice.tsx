d2bf6c6a5ed35008893096968d47e791363c631d
LeeYongwoo-kor <newlyo1205@naver.com>
2023-03-01 18:46:58 +0900

    Modify the design of the Notice component

--- components/Notice.tsx
+++ components/Notice.tsx
@@ -1,8 +1,11 @@
+import { phonenumber } from "@constants/common";
 import Link from "next/link";
 import { useState } from "react";
 import { CommonProps } from "types/CommonProps";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
 import Slider from "./Slider";
 import Subtitle from "./Subtitle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Notice({ innerRef }: CommonProps) {
   const [imageIndex, setImageIndex] = useState<number | null>(null);
@@ -10,37 +13,39 @@ export default function Notice({ innerRef }: CommonProps) {
     setImageIndex(childIndex);
   };
   return (
-    <article ref={innerRef} className="w-full min-h-fit font-mincho">
+    <article ref={innerRef} className="w-full min-h-fit">
       <h1 className="hidden">お知らせ</h1>
       <Subtitle text="NOTICE" />
-      <div className="flex flex-col items-center justify-center md:flex-row">
-        <div className="relative md:flex md:p-8 w-full sm:w-5/6 h-112 sm:h-128 mobile:px-4 md:h-[40rem] md:flex-3">
-          <Slider component="notice" callback={getIndex} />
+      <div className="flex flex-col items-center justify-center md:flex-row bg-slate-100">
+        <div className="relative md:flex pt-8 md:p-8 w-full sm:w-5/6 h-112 sm:h-128 mobile:px-4 md:h-[40rem] md:flex-3">
+          <Slider component="notice" callback={getIndex} intervalTimer={5000} />
         </div>
         {imageIndex !== null ? (
-          <div className="relative flex flex-col self-start p-8 md:pt-16 flex-2">
+          <div className="relative flex flex-col self-start p-8 md:pt-24 flex-2">
             {imageIndex === 0 ? (
               <>
-                <h2 className="mb-10 text-2xl font-semibold font-murecho">
+                <h2 className="mb-5 text-2xl font-bold md:mb-8">
                   お持ち帰り用の<span className="text-darkmain">卵サンド</span>
                   のご予約を承っております。
                 </h2>
                 <div>
-                  <p className="mb-8 text-lg font-semibold text-gray-600">
+                  <p className="mb-2 text-base font-bold md:text-lg md:mb-4 text-slate-700 font-mincho">
                     のりやん食堂では、自家製の卵サンドをお持ち帰りいただけます。
                     新鮮な卵をたっぷりと使用することで、フワッフワの食感に仕上がっております。
                     ぜひ、一度ご賞味ください。
                   </p>
                 </div>
+                <div>
+                  <p className="mb-5 text-xs font-medium md:text-sm md:mb-8 text-highlight">
+                    ※前日までご予約をお願いします。
+                  </p>
+                </div>
                 <div className="w-full">
                   <Link
-                    href={`tel:000`}
-                    className="inline-flex flex-col items-center w-full px-4 py-2 font-medium text-white transition-colors border border-transparent rounded-md shadow-sm cursor-pointer bg-highlight hover:bg-darkmain focus:outline-none focus:ring-2 focus:ring-darkmain focus:ring-offset-2"
+                    href={`tel:${phonenumber}`}
+                    className="inline-flex flex-col items-center w-full px-4 py-4 font-medium text-white transition-colors border border-transparent rounded-md shadow-sm cursor-pointer bg-highlight hover:bg-darkmain focus:outline-none focus:ring-2 focus:ring-darkmain focus:ring-offset-2"
                   >
-                    <div className="self-start text-sm font-bold">
-                      予約はこちら
-                    </div>
-                    <div className="flex items-center justify-center text-lg">
+                    <div className="flex items-center justify-center">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24"
@@ -53,9 +58,43 @@ export default function Notice({ innerRef }: CommonProps) {
                           clipRule="evenodd"
                         />
                       </svg>
-                      <div className="hidden ml-1 text-2xl md:block">
-                        052-937-6252
-                      </div>
+                      <div className="ml-2 text-lg">ご予約の方はこちら</div>
+                    </div>
+                  </Link>
+                </div>
+              </>
+            ) : null}
+            {imageIndex === 1 ? (
+              <>
+                <h2 className="mb-5 text-2xl font-bold md:mb-8">
+                  毎月新鮮な<span className="text-darkmain">季節料理</span>
+                  をご用意しております。
+                </h2>
+                <div>
+                  <p className="mb-2 text-base font-bold md:text-lg md:mb-4 text-slate-700 font-mincho">
+                    当店では、毎月旬の食材を使用した季節料理を提供しています。
+                    地元の食材にこだわり、素材の旨味を最大限に引き出す料理をご提供し、お客様にご満足いただける食堂を目指しています。
+                    是非、当店で美味しいお食事をお楽しみください。
+                  </p>
+                </div>
+                <div>
+                  <p className="mb-5 text-xs font-medium md:text-sm md:mb-8 text-highlight">
+                    ※売り切れる場合がございます。
+                  </p>
+                </div>
+                <div className="flex items-center justify-center w-full">
+                  <Link
+                    href="https://www.instagram.com/kurumamichi.noriyan/"
+                    target={"_blank"}
+                    className="inline-flex flex-col items-center justify-center w-full px-4 py-4 font-medium text-white transition-colors border border-transparent rounded-md shadow-sm cursor-pointer bg-highlight hover:bg-darkmain focus:outline-none focus:ring-2 focus:ring-darkmain focus:ring-offset-2"
+                  >
+                    <div className="flex items-center justify-center">
+                      <FontAwesomeIcon
+                        className="text-white"
+                        size="2x"
+                        icon={faInstagram}
+                      />
+                      <div className="ml-2 text-lg">最新の情報ははこちら</div>
                     </div>
                   </Link>
                 </div>
