ed9afa33f1fe5d3e32778f2d07e7933de47a6df8
LeeYongwoo-kor <newlyo1205@naver.com>
2023-03-07 23:58:40 +0900

    Adding information from the Common Change lowercase to uppercase

--- constants/common.ts
+++ constants/common.ts
@@ -1,4 +1,14 @@
-export const phonenumber = "0529376252";
-export const navHeight = 400;
-export const navbarMobileHeight = 64;
-export const tabletSize = 768;
+export const PHONE_NUMBER = "0529376252";
+export const EMAIL = "aoidayo210@gmail.com";
+export const NAV_HEIGHT = 400;
+export const NAVBAR_MOBILE_HEIGHT = 64;
+export const TABLET_SIZE = 769;
+export const DAY_TO_MILLISECOND = 86400000;
+export const DECISION = 21;
+
+export function getJpCurrency(price: number) {
+  return new Intl.NumberFormat("ja-JP", {
+    style: "currency",
+    currency: "JPY",
+  }).format(price);
+}
