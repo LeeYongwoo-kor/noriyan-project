3cb20b4858a1f585306a13fecfb54400772db1b1
LeeYongwoo-kor <newlyo1205@naver.com>
2023-03-08 03:45:39 +0900

    Modify the next.config.js Add domains, Setting image optimization, Add @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: ["norisang-project.s3.ap-northeast-1.amazonaws.com"],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [480, 640, 769, 1024, 1280, 1920],
+    domains: [
+      "norisang-project.s3.ap-northeast-1.amazonaws.com",
+      "d22suaf3v2qshw.cloudfront.net",
+      "dining-menu.com",
+    ],
   },
 };
 
-module.exports = nextConfig;
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [withBundleAnalyzer];
+  return plugins.reduce((acc, plugin) => plugin(acc), {
+    ...nextConfig,
+  });
+};
