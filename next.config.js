689b6abe4a9798012e618bb47b41cfe0f1d69200
LeeYongwoo-kor <newlyo1205@naver.com>
2023-03-11 21:50:04 +0900

    Make it possible to annalize only when in development mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+  enabled:
+    process.env.NODE_ENV === "development" && process.env.ANALYZE === "true",
 });
 
 /** @type {import('next').NextConfig} */
